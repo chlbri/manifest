97894001204644aa1d66ac12d2c9ea5a8bef379c
chlbri <51156116+chlbri@users.noreply.github.com>
2025-08-27 17:59:47 +0000

    Refactor tests in cli.test.ts for improved clarity and organization

--- src/cli/cli.test.ts
+++ src/cli/cli.test.ts
@@ -1,65 +1,76 @@
 import { run } from 'cmd-ts';
+import type { MockInstance } from 'vitest';
 import { cli } from './cli';
 import { cliTests, useBuild } from './fixtures';
 
 describe('CLI', () => {
   describe('#01 => Without options', () => {
     useBuild();
-    test('#01.02 => Generate manifest', () => run(cli, []));
-    describe(...cliTests('01.03', 'noOptions'));
+    test('#01 => Generate manifest', () => run(cli, []));
+    describe(...cliTests('02', 'noOptions'));
   });
 
   describe('#02 => with options', () => {
     useBuild();
 
-    describe('#02.01 => --const', () => {
-      describe('#02.01.01 => long', () => {
-        test('#02.01.01.01 => --const', () => run(cli, ['--const']));
-        describe(...cliTests('02.01.01.02', 'const'));
+    describe('#01 => --const', () => {
+      describe('#01 => long', () => {
+        test('#01 => --const', () => run(cli, ['--const']));
+        describe(...cliTests('01', 'const'));
       });
-      describe('#02.01.02 => short', () => {
-        test('#02.01.02.01 => -c', () => run(cli, ['-c']));
-        describe(...cliTests('02.01.02.02', 'const'));
-      });
-    });
-
-    describe('#02.02 => --exclude', () => {
-      describe('#02.02.01 => "cli/cli.ts"', () => {
-        test('#02.02.01.01 => cli', () =>
-          run(cli, ['--exclude', 'cli/cli.ts']));
-
-        describe(...cliTests('02.02', 'excludeCli'));
-      });
-
-      describe('#02.02.01 => "helpers.ts', () => {
-        test('#02.02.01.01 => cli', () => run(cli, ['-e', 'helpers.ts']));
-
-        describe(...cliTests('02.02', 'excludeHelpers'));
+      describe('#02 => short', () => {
+        test('#01 => -c', () => run(cli, ['-c']));
+        describe(...cliTests('02', 'const'));
       });
     });
 
-    describe('#02.03 => --verbose', () => {
-      describe('#02.03.01 => long', () => {
-        test('#02.03.01.01 => --verbose', () => run(cli, ['--verbose']));
-        describe(...cliTests('02.03.01.02', 'noOptions'));
+    describe('#02 => --exclude', () => {
+      describe('#01 => "cli/cli.ts"', () => {
+        test('#01 => cli', () => run(cli, ['--exclude', 'cli/cli.ts']));
+
+        describe(...cliTests('01', 'excludeCli'));
       });
-      describe('#02.03.02 => short', () => {
-        const spy = vi.spyOn(console, 'log');
-        test('#02.03.02.01 => -v', () => run(cli, ['-v']));
-        describe(...cliTests('02.03.02.02', 'noOptions'));
-        describe('#02.03.02 => console.log', () => {
-          test('#02.03.02.01 => Called with "🔍 Scan des fichiers..."', () => {
+
+      describe('#02 => "helpers.ts"', () => {
+        test('#01 => cli', () => run(cli, ['-e', 'helpers.ts']));
+
+        describe(...cliTests('02', 'excludeHelpers'));
+      });
+    });
+
+    describe('#03 => --verbose', () => {
+      let spy: MockInstance;
+      describe('#01 => long', () => {
+        beforeAll(() => (spy = vi.spyOn(console, 'log')));
+        test('#01 => --verbose', () => run(cli, ['--verbose']));
+        describe(...cliTests('#02', 'noOptions'));
+        describe('#03 => console.log', () => {
+          test('#01 => Called with "🔍 Scan des fichiers..."', () => {
             expect(spy).toHaveBeenCalledWith('🔍 Scan des fichiers...');
           });
 
-          test('#02.03.02.02 => Called with "🚫 Fichier exclu: .manifest.ts"', () => {
+          test('#02 => Called with "🚫 Fichier exclu: .manifest.ts"', () => {
             expect(spy).toHaveBeenCalledWith(
               '🚫 Fichier exclu: .manifest.ts',
             );
           });
         });
-        afterAll(() => {
-          spy.mockRestore();
+        afterAll(() => spy.mockClear());
+      });
+      describe('#02 => short', () => {
+        beforeAll(() => (spy = vi.spyOn(console, 'log')));
+        test('#01 => -v', () => run(cli, ['-v']));
+        describe(...cliTests('02', 'noOptions'));
+        describe('#02 => console.log', () => {
+          test('#01 => Called with "🔍 Scan des fichiers..."', () => {
+            expect(spy).toHaveBeenCalledWith('🔍 Scan des fichiers...');
+          });
+
+          test('#02 => Called with "🚫 Fichier exclu: .manifest.ts"', () => {
+            expect(spy).toHaveBeenCalledWith(
+              '🚫 Fichier exclu: .manifest.ts',
+            );
+          });
         });
       });
     });
